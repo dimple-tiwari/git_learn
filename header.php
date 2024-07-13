eaa9e7a411cfcabc166cfe9d547548dbf0868bb2
Dimple Tiwari <dimple@atyantik.com>
2024-07-14 01:02:37 +0530

    amend testing commit with add

--- header.php
+++ header.php
@@ -4,14 +4,6 @@
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+ */                                 
+ 
+ test11
